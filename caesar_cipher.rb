c03fe521f2d6230b9b01a77e56a462efb9ca007e
Ryan Lovell <ryanlovell@Ryans-MacBook-Pro.local>
2016-04-09 00:44:22 -0400

    added file i/o & decipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,38 +1,47 @@
 class CaesarCipher
 
-	attr_reader :string, :key
+	attr_reader :string, :key, :file
 
 	LCASE_BEGIN = "a".ord
 	UPCASE_BEGIN = "A".ord
 	LCASE_LAST = "z".ord
 	UPCASE_LAST = "Z".ord
 
-	def initialize( string, key)
-		@string = string
+	def initialize( file, key )
+		@file = get_text(file)
 		@key = key
 	end
 		
+	def get_text(file)
+		text = ""
+		File.open(file).each { |line| text << line }
+		text
+	end
+
+
 	def cipher
-		string_array.map { |char| char =~ /[a-z]|[A-Z]/ ? translate( char, @key ) : char }.join
+		cipher = string_array.map { |char| char =~ /[a-z]|[A-Z]/ ? translate( char, @key ) : char }.join
+		File.write("export.txt", cipher)
 	end
 
 	private
 
 		def string_array
-		@string.split("")
+		@file.split("")
 	end
 
 	def translate( letter, key )
 			order = (letter.ord + key)
 
-			if order > LCASE_LAST && letter == letter.downcase
-
+			case 
+			when order > LCASE_LAST && letter == letter.downcase
 				char_int = lower_case_limit( letter, order )
-
-			elsif order > UPCASE_LAST && letter == letter.upcase
-
+			when order > UPCASE_LAST && letter == letter.upcase
 				char_int = upper_case_limit( letter, order )
-
+			when order < LCASE_BEGIN && letter == letter.downcase
+				char_int = lower_case_begin( letter, order )
+			when order < UPCASE_BEGIN && letter == letter.upcase
+				char_int = upper_case_begin( letter, order )
 			else
 				char_int = order.chr
 			end
@@ -44,8 +53,16 @@ class CaesarCipher
 		((key - LCASE_LAST) + LCASE_BEGIN - 1 ).chr
 	end
 
+	def lower_case_begin( letter, key )
+		((key - LCASE_BEGIN) + LCASE_LAST + 1 ).chr
+	end
+
 	def upper_case_limit( letter, key )
 		((key - UPCASE_LAST) + UPCASE_BEGIN - 1 ).chr
+	end
+
+	def upper_case_begin( letter, key )
+		((key - UPCASE_BEGIN) + UPCASE_LAST + 1 ).chr
 	end
 
 end
